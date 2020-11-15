e73a29a179a5cb82417b1d3dcfbe305b8f436d8f
masmerino <masmerino@gmail.com>
2020-11-15 08:58:40 -0600

    Add css vars to the whole app

--- poi.config.js
+++ poi.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   reactRefresh: true,
   entry: 'src/index',
+  css: {
+    loaderOptions: {
+      // pass options to sass-loader
+      sass: {
+        // @/ is an alias to src/
+        // so this assumes you have a file named `src/variables.scss`
+        additionalData: `@import "@/variables.scss";`
+      }
+    }
+  }
 }
