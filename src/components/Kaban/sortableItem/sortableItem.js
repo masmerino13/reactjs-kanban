523b2d6fb799a5f13ef87b0c29dc52c078fed13c
masmerino <masmerino@gmail.com>
2020-11-20 22:01:14 -0600

    Show assignee user properly

--- src/components/Kaban/sortableItem/sortableItem.js
+++ src/components/Kaban/sortableItem/sortableItem.js
@@ -11,7 +11,7 @@ import './sortableItem.scss'
 const SortableItem = SortableElement(({ item }) => <div className='t_sortableItem'>
   <div className='t_sortableItem_Title'>{item.title}</div>
   <div className='t_sortableItemFooter'>
-    <UserShortName keyword='Ricardo Merino' />
+    <UserShortName keyword={item.assignee} />
 <div className='t_sortableItemFooter_Due'>Due: { formatDistance(item.due, new Date(2020, 11, 18)) }</div>
   </div>
 </div>)
