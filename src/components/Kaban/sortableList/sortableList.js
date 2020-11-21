2b367fbddd297f9b5ca0db079700e87e5c10c69f
masmerino <masmerino@gmail.com>
2020-11-21 00:07:28 -0600

    Add column badge

--- src/components/Kaban/sortableList/sortableList.js
+++ src/components/Kaban/sortableList/sortableList.js
@@ -2,33 +2,53 @@ import React from 'react'
 import {
   SortableContainer
 } from "react-sortable-hoc"
+import { Badge } from 'react-bootstrap'
 
 import { SortableItem } from '../sortableItem'
 
 import './sortableList.scss'
 
 const SortableList = SortableContainer(
-  ({ setIsHovering, collections }) => (
-    <div className='t_SortableList'>
-      {
-        collections.map(collection => (<div className='bg-light t_SortableListColumn'
-          key={collection.key}
-          onMouseEnter={() => setIsHovering(collection.key)}
-          onMouseLeave={() => setIsHovering('')}
-        >
-          <div className='t_SortableListColumn_Title'>{collection.title}</div>
-          {collection.items.map((item, index) => (
-            <SortableItem
-              key={`${collection.key}-${index}`}
-              index={index}
-              item={item}
-              collection={collection.key}
-            />
-          ))}
-        </div>))
+  ({ setIsHovering, collections }) => {
+    const setBadgeVarian = key => {
+      switch (key) {
+        case 'to-do':
+          return 'warning'
+        case 'in-progress':
+          return 'primary'
+        case 'done':
+          return 'success'
+        default:
+          return 'dark'
       }
-    </div>
-  )
+    }
+
+    return (
+      <div className='t_SortableList'>
+        {
+          collections.map(collection => {
+            return (
+              <div className='bg-light t_SortableListColumn'
+                key={collection.key}
+                onMouseEnter={() => setIsHovering(collection.key)}
+                onMouseLeave={() => setIsHovering('')}
+              >
+                <div className='t_SortableListColumn_Title'>{collection.title} <Badge className='t_SortableListColumn_Badge' variant={setBadgeVarian(collection.key)}>{collection.items.length}</Badge></div>
+                {collection.items.map((item, index) => (
+                  <SortableItem
+                    key={`${collection.key}-${index}`}
+                    index={index}
+                    item={item}
+                    collection={collection.key}
+                  />
+                ))}
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 )
 
 export default SortableList;
